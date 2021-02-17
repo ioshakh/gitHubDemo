b856c56970750b2f69cf8435121544d564f002fd
Shahzod <bektemirovshahzodbek@gmail.com>
2021-02-17 23:43:09 +0500

    Initial Commit

---
+++ gitHubDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubDemo
+//
+//  Created by shahzod on 2/17/21.
+//  Copyright © 2021 shahzod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
